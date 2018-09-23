0155285f6329ada9085db5e5d714282718daed01
kado10 <32041096+kado10@users.noreply.github.com>
2018-09-23 15:08:53 +0100

    formatted sections
    formatted sections

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
-main.js
+/*!
+ * jQuery JavaScript Library v3.2.1
+ * https://jquery.com/
+ *
+ * Includes Sizzle.js
+ * https://sizzlejs.com/
+ *
+ * Copyright JS Foundation and other contributors
+ * Released under the MIT license
+ * https://jquery.org/license
+ *
+ * Date: 2017-03-20T18:59Z
+ */
+
+$(function() {
+	$('.circle').mouseover(function() {
+		var cls = $(this).attr('id');
+		$('div.'+ cls).removeClass('hidden');
+	})
+	$('.circle').mouseout(function() {
+		var cls = $(this).attr('id');
+		$('div.'+ cls).addClass('hidden');
+	})
+});
+
+$(function() {
+	$('.circle_2').mouseover(function() {
+		var cls = $(this).attr('id');
+		$('div.'+ cls).removeClass('hidden');
+	})
+	$('.circle_2').mouseout(function() {
+		var cls = $(this).attr('id');
+		$('div.'+ cls).addClass('hidden');
+	})
+})
+
+// Initialize and add the map
+function initMap() {
+  // The location of dickensinn
+  var dickensinn = {lat: 51.507038, lng: -0.070199};
+  // The map, centered at dickensinn
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 16, center: dickensinn});
+  // The marker, positioned at dickensinn
+
+  var marker = new google.maps.Marker({position: dickensinn, map: map});
+}
+
+
+
+    
+  
